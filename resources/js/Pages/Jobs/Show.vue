9a1435b1fe1d7ed63721f64f2e8bb9e2ef629349
thevowels <aungphyohtet1@gmail.com>
2025-05-27 01:16:03 +0700

    feat: Showpage distinct from index as Jobcard is now displaying full description

--- resources/js/Pages/Jobs/Show.vue
+++ resources/js/Pages/Jobs/Show.vue
@@ -1,10 +1,26 @@
 <script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import JobCard from "@/Components/Jobs/JobCard.vue";
+
 defineProps({job: Object})
 </script>
 
 <template>
-   <p>Show Page for
-    {{job.title}}
-   </p>
-    ID: {{job.id}}
+    <AuthenticatedLayout>
+        <template #header>
+            <h2
+                class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200"
+            >
+                Jobs
+            </h2>
+        </template>
+        <div class="py-12">
+            <div class="mx-auto max-w-4xl sm:px-6 lg:px-8">
+                <JobCard :job="job">
+                    <p class="whitespace-pre-line">{{job.description}}</p>
+                    
+                </JobCard>
+            </div>
+        </div>
+    </AuthenticatedLayout>
 </template>
