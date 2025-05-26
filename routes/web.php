384e39a4fa5567d3a27f7e853f4e3068c81d2c9d
thevowels <aungphyohtet1@gmail.com>
2025-05-27 00:35:06 +0700

    feat: init shadcn-vue on project. fix: move /jobs route to auth middleware group

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('/jobs', JobController::class)->names('jobs')->only(['index', 'show']);
+
 });
 
-Route::resource('/jobs', JobController::class)->names('jobs')->only(['index', 'show']);
 
 require __DIR__.'/auth.php';
